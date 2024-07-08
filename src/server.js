14c295b8033da7546bbed8c989cb1c90207e4f2c
Stepan Lopushanskyi <stepanlopushanskyi@MacBook-Pro-Stepan.local>
2024-07-08 15:06:29 +0300

    create directory for files if does not exist

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
+const fs = require('node:fs');
 const checkConfig = require('./check_config');
 const appStart = require('./app');
+const path = require('node:path');
 
 checkConfig()
+  .then(() => {
+    const pathToDir = path.join(__dirname, '../', 'files/blocks')
+    if (!fs.existsSync(pathToDir)) {
+      fs.mkdirSync(pathToDir, { recursive: true });
+    }
+  })
   .then(appStart)
   .catch((error) => {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
